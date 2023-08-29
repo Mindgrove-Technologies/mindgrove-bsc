9b081a9b3e2ccf470b1794e14f4a0a4a2e69af68
MounaKrishna <mounakrishna@mindgrovetech.in>
2023-08-29 20:36:05 +0530

    Changed MakeReset0 to a general passthrough of reset

--- src/Verilog/MakeReset0.v
+++ src/Verilog/MakeReset0.v
@@ -32,33 +32,6 @@ module MakeReset0 (
 
    output             OUT_RST ;
 
-   reg                rst ;
-
-   assign ASSERT_OUT =  rst == `BSV_RESET_VALUE ;
-
-   assign OUT_RST = rst ;
-
-   always@(posedge CLK or `BSV_RESET_EDGE RST) begin
-      if (RST == `BSV_RESET_VALUE)
-        rst <= `BSV_ASSIGNMENT_DELAY init ? ~ `BSV_RESET_VALUE : `BSV_RESET_VALUE;
-      else
-        begin
-           if (ASSERT_IN)
-             rst <= `BSV_ASSIGNMENT_DELAY `BSV_RESET_VALUE;
-           else // if (rst == 1'b0)
-             rst <= `BSV_ASSIGNMENT_DELAY ~ `BSV_RESET_VALUE;
-        end // else: !if(RST == `BSV_RESET_VALUE)
-   end // always@ (posedge CLK or `BSV_RESET_EDGE RST)
-
-
-`ifdef BSV_NO_INITIAL_BLOCKS
-`else // not BSV_NO_INITIAL_BLOCKS
-   // synopsys translate_off
-   initial begin
-      #0 ;
-      rst = ~ `BSV_RESET_VALUE ;
-   end
-   // synopsys translate_on
-`endif // BSV_NO_INITIAL_BLOCKS
+   assign OUT_RST = ASSERT_OUT;
 
 endmodule // MakeReset0
